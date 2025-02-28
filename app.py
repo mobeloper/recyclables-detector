447cf0060def73c5a03a1d9e660e22fe4b46d29e
EricMichel <frack52@hotmail.com>
2025-02-28 00:25:15 -0800

    [add] predictor model for recyclables

---
+++ app.py
@@ -0,0 +1,11 @@
+import sys
+import tensorflow as tf
+import csv
+from typing import Any, TypedDict, Callable
+import cv2
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+logging.disable(logging.WARNING)
+
+%matplotlib inline
